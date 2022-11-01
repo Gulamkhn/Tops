2eab005779a143ce88803e56f12e00ff34c9bc8f
Gulam Khn <khngulam72@gmail.com>
2022-11-01 10:40:29 +0530

    New Commit

---
+++ shopapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('login/',views.login,name='login'),
+    path('register/',views.register,name='register'),
+    path('products/',views.products,name='products'),
+    path('addtocart/',views.addtocart,name='addtocart'),
+    path('cart/',views.cart,name='cart'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('single/<int:pid>',views.single,name='single'),
+    path('paymenthandler/', views.paymenthandler, name='paymenthandler'),
+    path('pay/',views.checkout,name='pay'),
+    
+]
